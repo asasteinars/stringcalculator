7ec3bdf78dc17e3de52435ab8983576cc4de8e79
asasteinars <asasteinars@gmail.com>
2014-10-17 14:47:49 +0000

    Lagaði newline testið

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -27,11 +27,12 @@ public class CalculatorTest {
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
+    	assertEquals(6, Calculator.add("1,2,3"));
     }
 
 	@Test
 	public void testNewLine() {
-		assertEquals(6, Calculator.add("1\n2,3"));
+		assertEquals(6, Calculator.add("1\n 2,3"));
 	}	
 
 }
