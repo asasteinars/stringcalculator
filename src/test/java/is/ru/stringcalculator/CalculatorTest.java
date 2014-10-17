3c8090e4e384d21bb7b67c42c792e4ba091ebed5
asasteinars <asasteinars@gmail.com>
2014-10-17 14:36:28 +0000

    Prófa að bæta við fleiri testu

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -20,16 +20,18 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testNewLine() {
-		assertEquals(6, Calculator.add("1\n2,3"));
-	}	
-	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(8, Calculator.add("4,4"));
 	}	
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+
+	@Test
+	public void testNewLine() {
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}	
 
 }
